c18b1f10c00a31674b3c5a184bed24da6ba4c708
dongdong-e <youngdong9800@gmail.com>
2019-11-05 19:38:54 +0900

    CustomAdmin

--- Django_manual/Djnago_Conceps.md
+++ Django_manual/Djnago_Conceps.md
@@ -3,9 +3,22 @@
 * **App:** function들의 집합
   * Room App: Room Search, Room Delete, Room Update...
 * **urls.py:** url 관리
-
 * **admin.py**:  'http://127.0.0.1:8000/admin/ '에 반영 (admin 패널에 반영)
 * **apps.py:** 'configuration' 파일
-
 * **models.py:** 'database'의 구조를 정의
+  * 'admin'의 User 메뉴에 'bio'라는 속성을 추가
+  * 속성값을 하나 추가할 경우, 이전 데이터들은 신규 속성값을 가지고 있지 않기 때문에 'default'값을 추가해줘야 함. 방법은 아래의 2가지.
+    1. models.TextField**(default="")** 또는
+    2. models.TextField**(none=True)**
+
+<br>
+
+![image](https://user-images.githubusercontent.com/42408554/68197768-1fb14c80-fffe-11e9-8669-2c8f74f68928.png)
+
+<br>
+
+![image](https://user-images.githubusercontent.com/42408554/68200683-b5030f80-0003-11ea-8497-b4326c2f8dcd.png)
+
+<br>
+
 * **views.py:** 사용자가 보게 될 화면
--- users/models.py
+++ users/models.py
@@ -3,4 +3,53 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+
+    """Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "eng"
+    LANGUAGE_KORREAN = "kor"
+    LANGUAGE_JAPANESE = "jpn"
+    LANGUAGE_CHINESE = "chn"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KORREAN, "Korean"),
+        (LANGUAGE_JAPANESE, "Japanese"),
+        (LANGUAGE_CHINESE, "Chinese"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+    CURRENCY_YEN = "yen"
+    CURRENCY_CNY = "cny"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
+        (CURRENCY_YEN, "YEN"),
+        (CURRENCY_CNY, "CNY"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
+    birthday = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
